f47720bff12e3eb163e4c05b8d8a782b442586fb
ym <ym@yuemei.com>
2020-04-15 10:31:27 +0800

    Initial commit

---
+++ Example/GMLFoundationExtension/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GMLFoundationExtension
+//
+//  Created by ym on 04/15/2020.
+//  Copyright (c) 2020 ym. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
